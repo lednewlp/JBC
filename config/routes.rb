5a61449afa3ad299c76bbff6dd20278f8bb29d69
Wendel Filho <wfilho@pop-os.localdomain>
2020-04-01 23:34:12 -0300

    JBC

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :contacts
+  resources :users, only: [:new, :create]
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "stick_page#index"
 
